db8a73d15775419a9ee5dfc29bc23615c65b45b6
Daan <dhanrajappu456@gamail.com>
2024-03-12 21:08:22 +0530

    update prot number in vue.config.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import cors from "cors";
 import "dotenv/config";
 import express, { Request, Response } from "express";
-
+const PORT = process.env.PORT || 5173;
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -10,6 +10,6 @@ app.get("/test", (req: Request, res: Response) => {
   res.json({ message: "hello" });
 });
 
-app.listen(7000, () => {
+app.listen(PORT, () => {
   console.log("Started the server!!!");
 });
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173, // Specify the port you want Vite to use
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
